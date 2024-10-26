9cc01c0922a341673181cb4c7a4f4e6824b1211f
marcosuper97 <gorpishin97@yandex.ru>
2024-10-27 01:16:08 +0300

    Финальное задание по 8 спринту

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.playlistmaker
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +21,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val search = findViewById<Button>(R.id.search_button)
+        val library = findViewById<Button>(R.id.library_button)
+        val settings = findViewById<Button>(R.id.settings_button)
+
+        search.setOnClickListener {
+            val searchIntent = Intent(this, Search::class.java)
+            startActivity(searchIntent)
+        }
+
+        library.setOnClickListener {
+            val libraryIntent = Intent(this, Library::class.java)
+            startActivity(libraryIntent)
+        }
+        settings.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
         }
     }
 }
