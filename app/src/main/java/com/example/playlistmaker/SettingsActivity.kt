e3348dee4e3b88a1a11483bca1f1161de1532dcb
marcosuper97 <gorpishin97@yandex.ru>
2024-11-05 20:44:55 +0300

    Закончил домашнее задание по стилям.

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
